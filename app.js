a50de8d9c6838d5fe4ac2f849ff4294fc6758952
Adrian Ross <adriantoddross@gmail.com>
2018-01-15 16:26:42 -0500

    elias solution

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // let name = 'Joe';
 // let age = 21;
 
-function whoAmI(name = 'Chris', age = 29){
- console.log('\"Hi, my name is ' + name + ' and I\'m ' + age + ' years old\"');
- return age;
+// function whoAmI(name = 'Chris', age = 29){
+//  console.log('\"Hi, my name is ' + name + ' and I\'m ' + age + ' years old\"');
+//  return age;
+// }
+
+// function birthYear(age = 29) {
+//   let yob = whoAmI();
+//   let yearOfBirth = 2018 - age;
+//   console.log('"I was born in ' + yearOfBirth + '\"');
+// }
+
+// birthYear();
+
+function yearOfBirth (age) {
+  const currentYear = 2018;
+  return currentYear - age;
 }
 
-function birthYear(age = 29) {
-  let yob=whoAmI();
-  let yearOfBirth = 2018 - age;
-  console.log('"I was born in ' + yearOfBirth + '\"');
+function whoAmI (name, age) {
+  const yob = yearOfBirth(age);
+  // return name + yob;
+  return `"Hi, my name is ${name} and I was born in ${yob}."`;
 }
 
-birthYear();
+console.log(whoAmI('Adrian', 24));
