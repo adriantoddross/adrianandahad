20cd588e5dc08d8cc1aa3399d1f09f60f2c9468f
Adrian Ross <adriantoddross@gmail.com>
2018-01-15 15:29:22 -0500

    stuck on whoIAm/yearOfBirth

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// let name = 'Joe';
+// let age = 21;
+
 function whoAmI(name = 'Chris', age = 29){
-  let yearOfBirth = 2018 - age;
+  let yob = age;
   console.log('\"Hi, my name is ' + name + ' and I\'m ' + age + ' years old\"');
-  console.log('"I was born in ' + yearOfBirth + '\"');
 }
 whoAmI();
 
+function birthYear(age = 29) {
+  let yearOfBirth = 2018 - age;
+  console.log('"I was born in ' + yearOfBirth + '\"');
+}
 
+birthYear();
